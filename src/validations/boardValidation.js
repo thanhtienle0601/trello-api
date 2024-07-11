d0810ae4455638540a2875594e7767cbd7bb0ec3
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-07-11 11:16:41 +0700

    validation with Joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,50 @@
+/**
+ * Updated by trungquandev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@trungquandev
+ * "A bit of fragrance clings to the hand that gives flowers!"
+ */
+import { StatusCodes } from 'http-status-codes'
+import Joi, { string } from 'joi'
+
+const createNew = async (req, res) => {
+  const conditionTrue = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title length must be at least 3 characters long',
+      'string.max':
+        'Title length must be less than or equal to 5 characters long',
+      'string.trim': 'Title must not have leading or trailing whitespace'
+    }),
+    description: Joi.string()
+      .required()
+      .min(3)
+      .max(250)
+      .trim()
+      .strict()
+      .messages({
+        'any.required': 'Description is required',
+        'string.empty': 'Description is not allowed to be empty',
+        'string.min': 'Description length must be at least 3 characters long',
+        'string.max':
+          'Description length must be less than or equal to 5 characters long',
+        'string.trim':
+          'Description must not have leading or trailing whitespace'
+      })
+  })
+
+  try {
+    // console.log('req.body: ', req.body)
+    await conditionTrue.validateAsync(req.body, { abortEarly: false })
+    // next()
+    res.status(StatusCodes.CREATED).json({ message: 'POST: API create boards' })
+  } catch (error) {
+    res
+      .status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ errors: new Error(error).message })
+  }
+}
+
+export const boardValidation = {
+  createNew
+}
