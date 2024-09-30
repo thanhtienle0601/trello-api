c6af460ed16f30c28f27142a7f9bfc0de64ce5d7
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-09-30 10:59:58 +0700

    Api create board

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,4 +1,5 @@
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 /**
  * Updated by trungquandev.com's author on August 17 2023
@@ -8,8 +9,13 @@ import { slugify } from '~/utils/formatters'
 const createNew = async (reqBody) => {
   // eslint-disable-next-line no-useless-catch
   try {
-    const newBoard = { ...reqBody, slug: slugify(reqBody.title) }
-    return newBoard
+    const data = { ...reqBody, slug: slugify(reqBody.title) }
+
+    return await boardModel.findOneById(
+      (
+        await boardModel.createOne(data)
+      ).insertedId
+    )
   } catch (error) {
     throw error
   }
