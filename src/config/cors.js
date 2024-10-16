47159bf6ad0e2314f1673c54747e92ef5bf199dc
thanhtienle0601 <thanhtienle0601@gmail.com>
2024-10-16 17:36:19 +0700

    Test whitelist domains

--- src/config/cors.js
+++ src/config/cors.js
@@ -21,7 +21,7 @@ export const corsOptions = {
 
     // Kiểm tra dem origin có phải là domain được chấp nhận hay không
     if (WHITELIST_DOMAINS.includes(origin)) {
-      return callback(null, true)
+      return callback(null, false)
     }
 
     // Cuối cùng nếu domain không được chấp nhận thì trả về lỗi
